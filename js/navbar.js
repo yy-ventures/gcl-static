829337636d3e8b8284abefc3fcdda03f74341283
Farhan21 <farhanabsar21@gmail.com>
2022-02-07 00:02:59 +0600

    navbar, responsive, active funtion

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,55 @@
-const navMenu = document.querySelector(".main-menu")
-const menuBtn = document.querySelector(".menu-btn")
+// navbar responsive function 
+const navSlide = function() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
 
-menuBtn.addEventListener("click", e => {
-    e.preventDefault()
-    navMenu.style.display = "block"
-})
+    //toggle Nav
+    burger.addEventListener("click", function() {
+        nav.classList.toggle("nav-active");
+
+        //animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
+            }
+        });
+        //burger menu
+        burger.classList.toggle("toggle");
+    });
+
+
+}
+navSlide();
+
+
+// navbar hover function
+
+const activeHover = () => {
+
+    const navParent = document.querySelector("#navMenu");
+    let currentLink = document.querySelector(`#navMenu a[href="${location.pathname}"]`);
+    
+    // adding a active class name
+    currentLink.classList.add("page-active");
+
+    // removing on hover
+    const hoverPageInactive = () => {
+        currentLink.classList.remove("page-active");
+    }
+    
+    // adding leave hover
+    const hoverPageActive = () => {
+        currentLink.classList.add("page-active");
+    }
+
+    // cycle through the children elements of navbar
+    for (let elem of navParent.children) {
+        elem.onmouseenter = hoverPageInactive;
+        elem.onmouseleave = hoverPageActive;
+    }
+}
+
+activeHover()
